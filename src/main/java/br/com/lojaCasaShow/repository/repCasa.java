095e61699b3acf6427803de30a73eed1e9f71471
Shoiti <shoiti.tokunaga@gmail.com>
2020-03-09 10:12:07 -0300

    1.1

--- src/main/java/br/com/lojaCasaShow/repository/repCasa.java
+++ src/main/java/br/com/lojaCasaShow/repository/repCasa.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.lojaCasaShow.domain.Casa;
-import br.com.lojaCasaShow.domain.Evento;
 
 public interface repCasa extends JpaRepository<Casa, Long>{
 	List<Casa> findByOrderByNomeAsc();
